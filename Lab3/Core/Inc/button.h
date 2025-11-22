f71f2a2a6c9b6895482c3d0e8fba76c4254de54e
TD <duc81.edu@gmail.com>
2025-11-22 10:42:23 +0700

    creat button

---
+++ Lab3/Core/Inc/button.h
@@ -0,0 +1,17 @@
+/*
+ * button.h
+ *
+ *  Created on: Nov 22, 2025
+ *      Author: TD
+ */
+
+#ifndef INC_BUTTON_H_
+#define INC_BUTTON_H_
+
+#define	NORMAL_STATE GPIO_PIN_SET
+#define PRESSED_STATE GPIO_PIN_RESET
+
+void getkeyInput();
+
+
+#endif /* INC_BUTTON_H_ */
---
+++ Lab3/Core/Src/button.c
@@ -0,0 +1,17 @@
+/*
+ * button.c
+ *
+ *  Created on: Nov 22, 2025
+ *      Author: TD
+ */
+
+
+#include "button.h"
+#include "main.h"
+int KeyReg0 = NORMAL_STATE;
+int KeyReg1 = NORMAL_STATE;
+int KeyReg2 = NORMAL_STATE;
+
+void getKeyInput(){
+
+}
